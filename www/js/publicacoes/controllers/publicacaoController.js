176360c74d55d8e6e1533b165642a0202b528962
Josafá Filho <josafafilho15@gmail.com>
2015-12-11 19:34:24 -0200

    Ajustando abertura do arquivo no iPhone

--- www/js/publicacoes/controllers/publicacaoController.js
+++ www/js/publicacoes/controllers/publicacaoController.js
@@ -2,96 +2,161 @@
  * Created by josafa on 25/10/15.
  */
 angular.module('pensando.publicacoes')
-    .controller('PublicacaoCtrl', function ($scope, $stateParams, $ionicLoading, PublicacaoFactory) {
+    .controller('PublicacaoCtrl', function ($scope, $stateParams, $ionicLoading, $ionicPopup, $timeout, $cordovaFile, $cordovaFileTransfer, $cordovaFileOpener2, PublicacaoFactory) {
 
         $scope.publicacao = {};
-        //$scope.targetPath = null;
-        //$scope.filename = null;
-        //$scope.progress = 0;
+        $scope.fileexists = false;
 
+        /**
+         * funções relativas ao carregamento dos dados da publicação
+         */
         $scope.loadPublicacao = function () {
             var publicacaoID = $stateParams.publicacaoID;
-
-            console.log(publicacaoID);
 
             PublicacaoFactory.getPublicacao(publicacaoID)
                 .then(loadPublicacaoSuccess, loadPublicacaoError);
         };
 
         function loadPublicacaoSuccess(response) {
-            console.log('success');
             $scope.publicacao = response.data;
             $ionicLoading.hide();
         }
 
         function loadPublicacaoError(error) {
-            console.log('error');
             $ionicLoading.hide();
-            alert("Ocorreu um erro ao carregar a publicação. Tente novamente mais tarde!");
+            $ionicPopup.alert({
+                title: 'Falha ao carregar publicação!',
+                template: "Ocorreu um erro ao carregar a publicação. Tente novamente mais tarde!"
+            });
             console.log(error);
         }
 
         $scope.loadPublicacao();
 
-        //$scope.download = function () {
-        //
-        //    document.addEventListener('deviceready', function () {
-        //        var url = $scope.publicacao.url;
-        //
-        //        $scope.filename = url.split("/").pop();
-        //
-        //        $scope.targetPath = cordova.file.externalRootDirectory + $scope.filename;
-        //
-        //        $cordovaFile.checkFile(cordova.file.externalRootDirectory, $scope.filename)
-        //            .then(function (success) {
-        //                $ionicPopup.alert({
-        //                    title: 'Arquivo baixado!',
-        //                    template: 'O arquivo já foi baixado. Utilize os botões abrir ou visualizar'
-        //                });
-        //            }, function (error) {
-        //                $ionicLoading.show({
-        //                    template: 'Carregando publicação...'
-        //                });
-        //
-        //                $cordovaFileTransfer.download(url, $scope.targetPath, {}, true).then(function (result) {
-        //                    $scope.progress = 100;
-        //                    $ionicLoading.hide();
-        //                }, function (error) {
-        //                    $ionicPopup.alert({
-        //                        title: 'Erro!',
-        //                        template: 'Ocorreu um erro ao efetuar o download da publicação'
-        //                    });
-        //                    $ionicLoading.hide();
-        //                    console.log('Error downloading');
-        //                    console.log(error);
-        //                }, function (progress) {
-        //                    $timeout(function () {
-        //                        $scope.progress = (progress.loaded / progress.total) * 100;
-        //                    });
-        //                });
-        //            }
-        //        );
-        //    });
-        //}
-        //
-        //$scope.open = function () {
-        //    document.addEventListener('deviceready', function () {
-        //        $cordovaFile.checkFile(cordova.file.externalRootDirectory, $scope.filename)
-        //            .then(function (success) {
-        //                $cordovaFileOpener2.open($scope.targetPath, 'application/pdf')
-        //                    .then(function () {
-        //                        console.log('Success opening');
-        //                    }, function (err) {
-        //                        alert('Erro ao abrir arquivo');
-        //                        console.log('Error opening: ' + JSON.stringify(err));
-        //                    }
-        //                );
-        //            }, function (error) {
-        //                alert('arquivo não existe');
-        //            });
-        //    });
-        //};
+        document.addEventListener('deviceready', function () {
 
-    }
-)
-;
+            /**
+             * funções relativas ao download da publicação
+             */
+            $scope.download = function () {
+                var dir = getPublicacaoDir();
+                var filename = getPublicacaoFilename();
+
+                if (dir && filename) {
+                    checkFile(dir, filename, fileAlreadyExists, fileNeedsDownload);
+                } else {
+                    $ionicPopup.alert({
+                        title: 'Falha ao fazer download do arquivo!',
+                        template: 'Plataforma não suportada o arquivo não será baixado.'
+                    });
+                }
+            };
+
+            function checkFile(dir, filename, onSuccess, onError) {
+                $cordovaFile.checkFile(dir, filename).then(onSuccess, onError);
+            }
+
+            function getPublicacaoDir() {
+                var pub_path = "pensando/publicacoes/";
+
+                if (ionic.Platform.isAndroid()) {
+                    return cordova.file.externalRootDirectory + pub_path;
+                } else if (ionic.Platform.isIOS()) {
+                    return cordova.file.documentsDirectory + pub_path;
+                }
+
+                return null;
+            }
+
+            function getPublicacaoFilename() {
+                if ($scope.publicacao && $scope.publicacao.volume) {
+                    return "volume-" + $scope.publicacao.volume + ".pdf";
+                }
+
+                return null;
+            }
+
+            function getPublicacaoFullPath() {
+                var dir = getPublicacaoDir();
+
+                if (dir) {
+                    return dir + getPublicacaoFilename();
+                }
+
+                return null;
+            }
+
+            function fileAlreadyExists(data) {
+                $scope.fileexists = true;
+                openPublicacao();
+            }
+
+            function fileNeedsDownload(error) {
+                downloadArquivoPublicacao();
+            }
+
+            function downloadArquivoPublicacao() {
+                $ionicLoading.show({
+                    template: 'Baixando publicação...'
+                });
+
+                $cordovaFileTransfer.download($scope.publicacao.url, getPublicacaoFullPath(), {}, true)
+                    .then(downloadSuccess, downloadError, downloadProgress);
+            }
+
+            function downloadSuccess(data) {
+                $ionicLoading.hide();
+                $scope.fileexists = true;
+                openPublicacao();
+            }
+
+            function downloadError(error) {
+                $ionicLoading.hide();
+                $ionicPopup.alert({
+                    title: 'Erro!',
+                    template: 'Ocorreu um erro ao efetuar o download da publicação'
+                });
+                console.log(error);
+            }
+
+            function downloadProgress(progress) {
+                $timeout(function () {
+                    $scope.progress = (progress.loaded / progress.total) * 100;
+                    console.log($scope.progress);
+                });
+            }
+
+            /**
+             * funções relativas a abertura do arquivo PDF
+             */
+            $scope.open = function () {
+                checkFile(getPublicacaoDir(), getPublicacaoFilename(), openPublicacao, openPublicacaoError);
+            };
+
+            function openPublicacao(data) {
+                $cordovaFileOpener2.open(getPublicacaoFullPath(), 'application/pdf').then(openPublicacaoSucess, openPublicacaoError);
+            }
+
+            function openPublicacaoSucess(data) {
+                //nothing to do
+            }
+
+            function openPublicacaoError(error) {
+                $ionicPopup.alert({
+                    title: 'Erro!',
+                    template: 'Ocorreu um erro ao abrir o arquivo'
+                });
+                console.log(error);
+            }
+
+        });
+
+
+        /**
+         * funções relativas ao compartilhamento da publicação
+         */
+        $scope.share = function () {
+            console.log("TODO: implementar...");
+        };
+
+    });
