5a9f7bc8821328a23dc7387e72923a08fed754f7
Josafá Filho <josafafilho15@gmail.com>
2016-01-07 16:19:47 -0200

    Adiciona link para visitar o portal na pagina sobre

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,3 +1,12 @@
 angular.module('pensando.controllers', [])
     .controller('PensandoCtrl', function ($scope, $ionicModal, $timeout) {
+
+        $scope.visit = function () {
+            $timeout(function () {
+                var url = "http://pensando.mj.gov.br";
+                window.open(encodeURI(url), '_system', 'location=yes');
+                return false;
+            })
+        };
+
     });
